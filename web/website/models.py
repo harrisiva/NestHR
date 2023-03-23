365f5dc71cda8f39a6cc78014270f6e43c687a0a
harrisiva <harrishiv6@gmail.com>
2023-03-23 18:12:47 -0400

    Setup basic views, templates, and created custom models to connect with AWS

--- web/website/models.py
+++ web/website/models.py
@@ -2,6 +2,8 @@ import mysql.connector # Using MySQL Connector rather than SQLAlchemy
 from . import private
 
 # From the previous assignments code (author: harrisiva)
+# TODO: Custom format function on values to get them in the required dict format
+# can use other SQL commands to get the right col names and dict size
 
 # General Database Handler (custom mysql connector wrapper) 
 class database:
@@ -13,7 +15,7 @@ class database:
             password=private.PASSWORD,
             database=self.name
         )
-        self.cursor = self.database.cursor()  # Create a cursor (thorugh which SQL commands are sent)
+        self.cursor = self.database.cursor()  # Create a cursor (through which SQL commands are sent)
         # TODO: Handle the different the views in here?
         return
 
@@ -54,23 +56,69 @@ class database:
         self.database.close()
         return
 
-class Organization():
-    def __init__(self): # Uses DB to load organization object's information
-        # has a database handler instance on its own
+# New Addressbook Handler
+class AddressBook():
+    def __init__(self, db:database):
+        self.database = db
+        return
+    def create_address(self):
+        # Uses the DB handlers insert function
+        return
+    def read_address(self,id): 
+        # Given a ID, return the address in a dictionary
+        query = f'SELECT * FROM addressbook WHERE ad_id={id}'
+        values = self.database.fetch(query, output=True)[0]
+        values = {
+            "street_num":values[3],
+            "unit_num":values[4],
+            "street_name":values[5],
+            "city":values[6],
+            "province":values[7],
+            "postal_code":values[8],
+            "country":values[9]
+        }
+        return values
+    def update_address(self,id):
+        return
+    def delete_address(self,id):
+        return
+
+# New Organization Handler
+class Organization(): # Used predominantly by the organization related views
+    def __init__(self, id=None): # Uses DB to load organization object's information
+        
+        # Has a database handler instance on its own which is passed to the addressbook (can maintain session use)
         self.database = database()
-        # id, name, and other basic stuff pulled from the ID
+        self.addressbook = AddressBook(self.database)
+       
+        self.id = id
+        # get the set other information from the database
+        query = f'SELECT * FROM organizations WHERE org_id={self.id}'
+        values = self.database.fetch(query,output=True)[0]
+        if len(values)<=0: print("error, organization ID invalid. May have to create a new organization")
+        else:
+            self.reg:str = str(values[3])
+            self.name:str = str(values[4])
+            self.balance:float = float(values[5])
+            self.ad_id:int = int(values[6])
+            self.desc:str = str(values[7])
+
+            # get and set the address as a dictionary
+            self.address:dict = self.addressbook.read_address(self.ad_id)
         return
     
     # Organization CRUD
-    def read_organizations(self):
-        # view current organization's data (incl. self and more)
-        return
+    def read_organizations(self): # TODO: Can also incldue sum of employees, tranasactional balance, and other summary type info
+        return {"reg":self.reg,"name":self.name,"balance":self.balance,"address":self.address,"desc":self.desc}
+    
     def create_organization(self):
         # overrrides self data (which would be null)
-        return    
+        return
+    
     def update_organization(self):
         # Updates in DB and seld
         return
+    
     def delete_organization(self):
         # Deletes the current organization object (ensures cascade condition is met)
         return
@@ -88,13 +136,16 @@ class Organization():
     # Employee CRUD (incl. ) (filters using the current organization)
     def create_employee(self):
         return
-    def read_employees(self):
-        return 
+    
+    def read_employees(self): # uses org_id and db handler to view all the employees in the current organization  
+        return self.database.fetch(f'SELECT * FROM employee WHERE org_id={self.id};', output=True) 
+    
     def update_employee(self): # Update multi games
         return
     def delete_employee(self):
         return
     
+    # TODO:
     # Department CRUD
     # Project CRUD
 
--- web/website/views.py
+++ web/website/views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template
-from .models import database
+from .models import Organization
 
 # Create a blueprint and give the blueprint a name
 views = Blueprint('views', __name__) # Way to seperate app out (layout of URLs)
@@ -11,21 +11,26 @@ def home():
         # Login to existing organization
         # Sign up
 
-    db_handler = database()  #Handler created for each sessionßß
-    data = db_handler.viewTables(output=True)
-    print(type(data))
-    print(data)
-    print(type(data[0])) 
-    return render_template('base.html', data=data)
+    organization = Organization(1) # Initialize new organization object
+    # get the required data and append it into the data list in its most attomic format
+    # required data:
+        # general organization details
+        # special queries data
+    return render_template('base.html')
 
 @views.route('/organization-dashboard')
 def organization(): # Restricted to any particular organization based on the session ID?
     # Dashboard for the organization, esentially the organization landing page/view
-    # Show data from special queries
-    # Show buttons to go to crud features
-    # Show organizations basic details (access level based) (SELECT where ORG name matches)
-    # Has redirect to page with CRUD features    
-    return 
+    # Import basic organization data and special query data
+    organization = Organization(1) # Handle using POST requests
+    profile = organization.read_organizations()
+    employees = organization.read_employees()
+    print(profile)
+    return render_template("organization.html", profile=profile,employees=employees)
+
+@views.route("/manage-employees")
+def manage_employees():
+    return render_template("manage-employees.html")
 
 @views.route("/employee-dashboard")
 def employee():
@@ -34,7 +39,4 @@ def employee():
     # Show data about the payroll
     # Show data about performance (restricted for the current employee only)
     # Has redirect to page with CRUD features
-    
-    # 
-    
     return
